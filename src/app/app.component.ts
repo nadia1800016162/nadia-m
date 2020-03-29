fedc24926b2e0389a802c97e94ad07aa333184b4
unknown <nadia1800016162@webmail.uad.ac.id>
2020-03-29 11:42:57 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './halaman-baru.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nadia1800016137';
+}
